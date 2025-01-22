a93e61e41a96c2308012cda6f8aca823d35b927f
chr1swill <alexanderwilliams20003@gmail.com>
2025-01-22 16:44:01 -0500

    feat: ensure state of audio is stored

--- script.js
+++ script.js
@@ -1,45 +1,119 @@
 (function FileInput() {
-  let inputEl;
-  let audioEl;
+   const inputEl = document.getElementById("file_picker");
+   const audioEl = document.getElementById("audio");
 
-  if ((inputEl = document.getElementById("file_picker")) === null) {
+  if (inputEl === null) {
     throw new ReferenceError(
       "Error no element with id: #file_picker"); 
   }
 
-  if ((audioEl = document.getElementById("audio")) === null) {
+  if (audioEl === null) {
     throw new ReferenceError(
       "Error no element with id: #audio"); 
   }
 
-  inputEl.addEventListener("change",
-    loadFile);
+  inputEl.addEventListener("change", async function(e) { 
+    await processFile(e);
+  });
 
 
-  /**@param{Event}*/
-  function loadFile(event) {
-    let file;
-    let fileUrl;
+  /**@returns{string}*/
+  async function calculateChecksum(file) {
+    const start = Date.now();
+    const arrayBuffer = await file.arrayBuffer();
+    const hashBuffer = await crypto.subtle.digest('SHA-1', arrayBuffer);
+    const end = Date.now();
+    console.log("time to make hashbuff: ", (end - start));
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    const checksum = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return checksum;
+  }
 
-    file = event.target.files[0];
+  /**@returns {string}*/
+  async function makeObjUrl(file) {
+    return URL.createObjectURL(file);
+  }
+
+  /**@returns {void}*/
+  function freeObjUrl(objUrl) {
+    URL.revokeObjectURL(objUrl);
+  }
+
+  /**@returns{string}*/
+  function makeStorageKey(filename, checksum) {
+    return filename + "-" + checksum;
+  }
+
+  function debounceStoreCurrentTime(storageKey, mediaElement) {
+    let tm = null;
+    const DEBOUNCE_DURATION = 5000;
+
+    function storeCurrentTime(storageKey, mediaElement) {
+      const time = mediaElement.currentTime;
+      console.log("storing currentTime: ", time);
+      localStorage.setItem(storageKey, time.toString());
+    }
+
+    function debounce() {
+      if (tm) clearTimeout(tm);
+
+      if (mediaElement.paused === false) {
+        storeCurrentTime(storageKey, mediaElement);
+        tm = setTimeout(debounce, DEBOUNCE_DURATION);
+      } 
+    }
+
+    mediaElement.onplay = function() {
+      debounce();
+    }
+    mediaElement.onpause = function() {
+      if (tm) clearTimeout(tm);
+      storeCurrentTime(storageKey, mediaElement);
+    }
+    mediaElement.onended = function() {
+      if (tm) clearTimeout(tm);
+      storeCurrentTime(storageKey, mediaElement);
+    }
+    window.onvisibilitychange = function() {
+      storeCurrentTime(storageKey, mediaElement);
+    }
+  }
+
+  /**
+   * @param{Event}
+   * @retuns{void}
+   */
+  async function processFile(event) {
+    const file = event.target.files[0];
     if (!file || !(file.type = "audio/wav")) {
       console.error("Error loading file: please provide a valid .wav file.");
       return;
     }
+    console.log("file: ", file);
 
-    fileUrl = URL.createObjectURL(file);
-    audioEl.src = fileUrl;
+    Promise.all([calculateChecksum(file), makeObjUrl(file)]).then(function(results) {
+      const checksum = results[0];
+      const objUrl = results[1];
+      console.log("checksum: ", checksum);
+      console.log("objUrl: ", objUrl);
 
-    audioEl.play.then(function() {
-      console.log("Audio is playing I think.?!");
-      URL.revokeObjectURL(fileUrl);
-    }).catch(function(err) {
-      console.error("Error: ", err);
+      const storageKey = makeStorageKey(file.name, results[0]);
+      console.log("storageKey: ", storageKey);
+      console.log("audioEl.currentTime: ", audioEl.currentTime); 
+
+      audioEl.src = objUrl;
+
+      const currentTime = localStorage.getItem(storageKey);
+      if (currentTime !== null) {
+        audioEl.currentTime = parseFloat(currentTime);
+      } 
+
+      debounceStoreCurrentTime(storageKey, audioEl);
+      console.log("ready to play audio");
+      return;
+    }).catch(function(error) {
+      console.error(error);
+      return;
     });
   }
-
-  // TODO: keep track of the currentTime in local storage peridcally 
-  // then when you open up the same file name again you can set currentTime
-  // to be value saved in localStorage
-  // effectivey saving the state of the audio book for the next time you listen
 })();
