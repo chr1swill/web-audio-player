e4dd18831c4c7e702221881b5b9762a8c265c29d
chr1swill <alexanderwilliams20003@gmail.com>
2025-01-26 19:02:45 -0500

    feat: implement reading in RiffChunk.chunkId

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,8 @@
 "use strict";
+function RiffChunkPrint(rc) {
+    console.log("rc.chunkId: ", String.fromCharCode(...rc.chunkId));
+    //console.log("rc.ckSize: ", ...rc.ckSize);
+}
 (function main() {
     var inputEl = document.getElementById("file_picker");
     if (inputEl === null) {
@@ -11,6 +15,27 @@
             return;
         }
         console.log("files: ", file);
-        return;
+        var fr = new FileReader();
+        fr.onerror = function (e) {
+            console.error("Error occured reading file: ", fr.error);
+            return;
+        };
+        fr.onload = function (e) {
+            const arrayBuffer = fr.result;
+            console.log("ArrayBuffer: ", arrayBuffer);
+            //const view: DataView  = new DataView(arrayBuffer);
+            //console.log("view: ", view);
+            //console.log("view.getInt8(0): ", String.fromCharCode(view.getInt8(0)));
+            //console.log("view.getInt8(1): ", String.fromCharCode(view.getInt8(1)));
+            //console.log("view.getInt8(3): ", String.fromCharCode(view.getInt8(3))); 
+            //console.log("view.getInt8(2): ", String.fromCharCode(view.getInt8(2)));
+            const rc = {
+                chunkId: new Uint8Array(arrayBuffer.slice(0, 4), 0, 4),
+                //ckSize: new Uint32Array(arrayBuffer.slice(5, 9), 0, 1),
+            };
+            RiffChunkPrint(rc);
+            return;
+        };
+        fr.readAsArrayBuffer(file);
     };
 })();
--- web/js/script.ts
+++ web/js/script.ts
@@ -1,3 +1,13 @@
+interface RiffChunk {
+  chunkId: Uint8Array;
+  //ckSize: Uint32Array;
+}
+
+function RiffChunkPrint(rc: RiffChunk): void {
+  console.log("rc.chunkId: ", String.fromCharCode(...rc.chunkId));
+  //console.log("rc.ckSize: ", ...rc.ckSize);
+}
+
 (function main() {
   var inputEl = document.getElementById("file_picker") as HTMLInputElement | null;
   if (inputEl === null) {
@@ -12,8 +22,35 @@
       console.error("Error loading file: please provide a valid .wav file.");
       return;
     }
-
     console.log("files: ", file);
-    return;
+
+    var fr: FileReader = new FileReader(); 
+
+    fr.onerror = function(e: Event) {
+      console.error("Error occured reading file: ", fr!.error);
+      return;
+    }
+    fr.onload = function(e: Event) {
+      const arrayBuffer = fr!.result as ArrayBuffer;
+      console.log("ArrayBuffer: ", arrayBuffer);
+      //const view: DataView  = new DataView(arrayBuffer);
+      //console.log("view: ", view);
+
+      //console.log("view.getInt8(0): ", String.fromCharCode(view.getInt8(0)));
+      //console.log("view.getInt8(1): ", String.fromCharCode(view.getInt8(1)));
+      //console.log("view.getInt8(3): ", String.fromCharCode(view.getInt8(3))); 
+      //console.log("view.getInt8(2): ", String.fromCharCode(view.getInt8(2)));
+
+      const rc: RiffChunk = {
+        chunkId: new Uint8Array(arrayBuffer.slice(0,4), 0, 4),
+        //ckSize: new Uint32Array(arrayBuffer.slice(5, 9), 0, 1),
+      };
+
+      RiffChunkPrint(rc);
+
+      return;
+    }
+
+    fr.readAsArrayBuffer(file);
   };
 })();
