2dcbcb26c780fac62d427c66398f0a8842c427a4
chr1swill <alexanderwilliams20003@gmail.com>
2025-01-27 17:54:09 -0500

    feat: correctly calculate duration of wav file in seconds

--- web/js/script.js
+++ web/js/script.js
@@ -1,12 +1,48 @@
 "use strict";
-function RiffChunkPrint(rc) {
-    console.log("rc.chunkId: ", String.fromCharCode(...rc.chunkId));
-    //console.log("rc.ckSize: ", ...rc.ckSize);
-}
 (function main() {
     var inputEl = document.getElementById("file_picker");
     if (inputEl === null) {
         throw new ReferenceError("Error no element with id: #file_picker");
+    }
+    function getWavDurationInSeconds(wavBuffer) {
+        const dataView = new DataView(wavBuffer);
+        const numChannels = dataView.getUint16(22, true); // nChannels byte 22
+        const sampleRate = dataView.getUint32(24, true); // sampleRate byte 24
+        const bitsPerSample = dataView.getUint16(34, true); // bitsPerSample byte 34
+        // Initialize variables for data chunk size and fact chunk size
+        let dataChunkSize = 0;
+        let hasFactChunk = false;
+        const CHUNK_OFFSET_AFTER_FMT = 0x26;
+        let cursor = CHUNK_OFFSET_AFTER_FMT;
+        const chunkId = new Uint8Array(wavBuffer.slice(CHUNK_OFFSET_AFTER_FMT, CHUNK_OFFSET_AFTER_FMT + 4), 0, 4);
+        console.log(`chunkId=(${chunkId})`);
+        const chunkIdStr = String.fromCharCode(...chunkId);
+        console.log(`chunkIdStr=(${chunkIdStr})`);
+        if (chunkIdStr === 'data') {
+            cursor += 4; // move to ckSize
+            dataChunkSize = dataView.getUint32(cursor, true);
+        }
+        else if (chunkIdStr === 'fact') {
+            hasFactChunk = true;
+            cursor += 4; // move cursor past 'fact' byte
+            const LEN = wavBuffer.byteLength - cursor;
+            for (; cursor < LEN; cursor++) {
+                let maybeChunkId = new Uint8Array(wavBuffer.slice(cursor, cursor + 4), 0, 4);
+                if (String.fromCharCode(...maybeChunkId) !== 'data') {
+                    continue;
+                }
+                else {
+                    cursor += 4; // move to ckSize
+                    dataChunkSize = dataView.getUint32(cursor, true);
+                    break;
+                }
+            }
+        }
+        else {
+            throw new ReferenceError(`No hanlder for chunk type ${String.fromCharCode(...chunkId)}`);
+        }
+        const duration = dataChunkSize / (sampleRate * numChannels * (bitsPerSample / 8));
+        return duration;
     }
     inputEl.onchange = function (e) {
         const file = inputEl !== null && inputEl.files ? inputEl === null || inputEl === void 0 ? void 0 : inputEl.files[0] : null;
@@ -23,17 +59,9 @@ function RiffChunkPrint(rc) {
         fr.onload = function (e) {
             const arrayBuffer = fr.result;
             console.log("ArrayBuffer: ", arrayBuffer);
-            //const view: DataView  = new DataView(arrayBuffer);
-            //console.log("view: ", view);
-            //console.log("view.getInt8(0): ", String.fromCharCode(view.getInt8(0)));
-            //console.log("view.getInt8(1): ", String.fromCharCode(view.getInt8(1)));
-            //console.log("view.getInt8(3): ", String.fromCharCode(view.getInt8(3))); 
-            //console.log("view.getInt8(2): ", String.fromCharCode(view.getInt8(2)));
-            const rc = {
-                chunkId: new Uint8Array(arrayBuffer.slice(0, 4), 0, 4),
-                //ckSize: new Uint32Array(arrayBuffer.slice(5, 9), 0, 1),
-            };
-            RiffChunkPrint(rc);
+            const view = new DataView(arrayBuffer);
+            const durationInSeconds = getWavDurationInSeconds(arrayBuffer);
+            console.log(`durationInSeconds=${durationInSeconds}`);
             return;
         };
         fr.readAsArrayBuffer(file);
--- web/js/script.ts
+++ web/js/script.ts
@@ -1,18 +1,57 @@
-interface RiffChunk {
-  chunkId: Uint8Array;
-  //ckSize: Uint32Array;
-}
-
-function RiffChunkPrint(rc: RiffChunk): void {
-  console.log("rc.chunkId: ", String.fromCharCode(...rc.chunkId));
-  //console.log("rc.ckSize: ", ...rc.ckSize);
-}
-
 (function main() {
   var inputEl = document.getElementById("file_picker") as HTMLInputElement | null;
   if (inputEl === null) {
     throw new ReferenceError(
       "Error no element with id: #file_picker");
+  }
+
+  function getWavDurationInSeconds(wavBuffer: ArrayBuffer): number {
+    const dataView = new DataView(wavBuffer);
+
+    const numChannels = dataView.getUint16(22, true); // nChannels byte 22
+    const sampleRate = dataView.getUint32(24, true); // sampleRate byte 24
+    const bitsPerSample = dataView.getUint16(34, true); // bitsPerSample byte 34
+
+    // Initialize variables for data chunk size and fact chunk size
+    let dataChunkSize = 0;
+    let hasFactChunk = false;
+
+    const CHUNK_OFFSET_AFTER_FMT = 0x26;
+    let cursor = CHUNK_OFFSET_AFTER_FMT;
+
+    const chunkId = new Uint8Array(wavBuffer.slice(CHUNK_OFFSET_AFTER_FMT, CHUNK_OFFSET_AFTER_FMT+4), 0, 4);
+    console.log(`chunkId=(${chunkId})`, );
+
+    const chunkIdStr = String.fromCharCode(...chunkId);
+    console.log(`chunkIdStr=(${chunkIdStr})`);
+
+    if (chunkIdStr === 'data') {
+      cursor+=4 // move to ckSize
+      dataChunkSize = dataView.getUint32(cursor, true);
+    } else if (chunkIdStr === 'fact') {
+      hasFactChunk = true;
+      cursor+=4; // move cursor past 'fact' byte
+
+      const LEN = wavBuffer.byteLength - cursor;
+
+      for (;cursor < LEN; cursor++) {
+        let maybeChunkId = new Uint8Array(wavBuffer.slice(cursor, cursor+4), 0, 4);
+        if (String.fromCharCode(...maybeChunkId) !== 'data') {
+          continue;
+        } else {
+          cursor+=4; // move to ckSize
+          dataChunkSize = dataView.getUint32(cursor, true);
+          break;
+        }
+      }
+
+    } else {
+      throw new ReferenceError(`No hanlder for chunk type ${String.fromCharCode(...chunkId)}`);
+    }
+
+    const duration = dataChunkSize / (sampleRate * numChannels * (bitsPerSample / 8));
+
+    return duration;
   }
 
   inputEl.onchange = function(e: Event): void {
@@ -33,20 +72,10 @@ function RiffChunkPrint(rc: RiffChunk): void {
     fr.onload = function(e: Event) {
       const arrayBuffer = fr!.result as ArrayBuffer;
       console.log("ArrayBuffer: ", arrayBuffer);
-      //const view: DataView  = new DataView(arrayBuffer);
-      //console.log("view: ", view);
+      const view = new DataView(arrayBuffer);
 
-      //console.log("view.getInt8(0): ", String.fromCharCode(view.getInt8(0)));
-      //console.log("view.getInt8(1): ", String.fromCharCode(view.getInt8(1)));
-      //console.log("view.getInt8(3): ", String.fromCharCode(view.getInt8(3))); 
-      //console.log("view.getInt8(2): ", String.fromCharCode(view.getInt8(2)));
-
-      const rc: RiffChunk = {
-        chunkId: new Uint8Array(arrayBuffer.slice(0,4), 0, 4),
-        //ckSize: new Uint32Array(arrayBuffer.slice(5, 9), 0, 1),
-      };
-
-      RiffChunkPrint(rc);
+      const durationInSeconds = getWavDurationInSeconds(arrayBuffer);
+      console.log(`durationInSeconds=${durationInSeconds}`); 
 
       return;
     }
